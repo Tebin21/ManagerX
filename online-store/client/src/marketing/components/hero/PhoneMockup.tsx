016274aa31490216ffcdc1a79e087518d08a3953
Tebin21 <tebin.232375@univsul.edu.iq>
2026-07-11 15:54:27 +0300

    Improve hero phone mockup size and premium frame

--- online-store/client/src/marketing/components/hero/PhoneMockup.tsx
+++ online-store/client/src/marketing/components/hero/PhoneMockup.tsx
@@ -2,17 +2,17 @@ import { ScreenshotCarousel } from "./ScreenshotCarousel";
 
 export function PhoneMockup() {
   return (
-    <div className="relative w-[150px] sm:w-[190px] md:w-[220px] lg:w-[260px]">
+    <div className="relative w-[190px] md:w-[220px] lg:w-[260px]">
       {/* Side buttons */}
-      <span className="absolute -left-[2px] top-[68px] h-6 w-[2px] rounded-l-sm bg-ink/90 sm:top-[84px] sm:h-7" />
-      <span className="absolute -left-[2px] top-[100px] h-10 w-[2px] rounded-l-sm bg-ink/90 sm:top-[124px] sm:h-12" />
-      <span className="absolute -right-[2px] top-[92px] h-14 w-[2px] rounded-r-sm bg-ink/90 sm:top-[114px] sm:h-16" />
+      <span className="absolute -left-[2px] top-[84px] h-7 w-[2px] rounded-l-sm bg-ink/90" />
+      <span className="absolute -left-[2px] top-[124px] h-12 w-[2px] rounded-l-sm bg-ink/90" />
+      <span className="absolute -right-[2px] top-[114px] h-16 w-[2px] rounded-r-sm bg-ink/90" />
 
-      <div className="rounded-[2.4rem] border-[8px] border-ink bg-ink p-1 shadow-2xl shadow-black/30 sm:rounded-[2.6rem] sm:border-[10px]">
-        <div className="relative aspect-[643/1280] overflow-hidden rounded-[1.9rem] bg-white sm:rounded-[2rem]">
+      <div className="rounded-[2.4rem] border-[4px] border-ink bg-ink p-0.5 shadow-2xl shadow-black/30 md:rounded-[2.6rem] md:border-[5px]">
+        <div className="relative aspect-[643/1280] overflow-hidden rounded-[2.15rem] bg-white md:rounded-[2.3rem]">
           <ScreenshotCarousel startIndex={0} priority className="h-full w-full" />
           {/* Dynamic island */}
-          <div className="pointer-events-none absolute left-1/2 top-2 z-10 h-4 w-16 -translate-x-1/2 rounded-full bg-ink sm:h-5 sm:w-20" />
+          <div className="pointer-events-none absolute left-1/2 top-2 z-10 h-5 w-20 -translate-x-1/2 rounded-full bg-ink" />
         </div>
       </div>
     </div>
